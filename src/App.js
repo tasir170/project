1682713f8e2e97e705654af880b32a44a04bb390
tasir170 <tasir.atd@gmail.com>
2020-08-06 14:47:45 +0500

    npm project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      This is practice session of script.147
+    </div>
+  );
+}
+
+export default App;
