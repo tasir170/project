f9f60ff16c7874e359cc7d0ffc982966c75b0bb2
tasir170 <tasir.atd@gmail.com>
2020-08-07 10:28:39 +0500

    dinner  react work

---
+++ src/Dinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function Dinner {}{
+    return(
+        <div>
+        <h1> today we are severing biryani </h1>
+        <h1> today we are severing karahi </h1>
+    </div>
+        )
+
+}
+export default Dinner;
