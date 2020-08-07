617e14645b405da8145c8c3272eb060ca9625e66
tasir170 <tasir.atd@gmail.com>
2020-08-07 10:41:34 +0500

    1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Dinner from 'Dinner';
+import Dinner from './Dinner';
 
 function App() {
-  return (
-    <div >
-      This is practice session of script. its time of party.
+  return (alias) function Dinner(); JSX.Element 
+    <div ClassName='app'>
+      <Dinner /> 
     </div>
   );
 }
