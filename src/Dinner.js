1d5b7f6e2fad1d2ae29bef640191737178aefa0f
tasir170 <tasir.atd@gmail.com>
2020-08-07 11:20:45 +0500

    5

--- src/Dinner.js
+++ src/Dinner.js
@@ -3,7 +3,7 @@ function Dinner(props){
     return(
         <div>
         <h1> today we are severing {props.dishName} </h1>
-        <h1> today we are severing{props.sweetDish}</h1>
+        <h1> today we are severing {props.sweetDish}</h1>
        
         <br />
     </div>
